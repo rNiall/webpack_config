942a8911ef9ee27ec4cb35ebd590c6cc3203ade1
Naill <2jetsetter@gmail.com>
2017-05-23 19:47:13 +0300

    fix problems with NODE_ENV (not the latest version of webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,49 @@
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack'); // webpack itself
+const path = require('path'); // nodejs dependency when dealing with paths
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // require webpack plugin
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // require webpack plugin
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin'); // require webpack plugin
 
-let config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './public'),
-        filename: 'output.js'
+let config = { // config object
+
+    entry: './src/index.js', // entry file
+    output: { // output
+        path: path.resolve(__dirname, './public'), // ouput path
+        filename: 'output.js' // output filename
     },
     module: {
-        rules: [
+        rules: [ // loader rules
             {
-                test: /\.js$/, // file endings with .js
+                test: /\.js$/, // files ending with .js
                 exclude: /node_modules/, // exclude the node_modules directory
-                loader: "babel-loader" // use this (babel-core) loader
+                loader: 'babel-loader' // use this (babel-core) loader
             },
             {
-                test: /\.scss$/, // files endings with .scss
+                test: /\.scss$/, // files ending with .scss
                 use: ExtractTextWebpackPlugin.extract({ // call our plugin with extract method
-                    use: ['css-loader', 'sass-loader'], // use this loaders
+                    use: ['css-loader', 'sass-loader'], // use these loaders
                     fallback: 'style-loader' // fallback for any CSS not extracted
                 }) // end extract
             }
         ] // end rules
     },
-    plugins: [
-        new ExtractTextWebpackPlugin('style.css') // call the ExtractTextWebpackPlugin constructor and name our css file
+    plugins: [ // webpack plugins
+        new ExtractTextWebpackPlugin('styles.css'), // call the ExtractTextWebpackPlugin constructor and name our css file
     ],
     devServer: {
         contentBase: path.resolve(__dirname, './public'), // A directory or URL to serve HTML content from.
-        historyApiFallback: true, // fallback to /index.html for SPA.
+        historyApiFallback: true, // fallback to /index.html for Single Page Applications.
         inline: true, // inline mode (set to false to disable including client scripts (like livereload)
         open: true // open default browser while launching
     },
-    devtool: 'eval-source-map' // enable devtool for better debugging experience
+    devtool: 'eval-source-map', // enable devtool for better debugging experience
 };
 
 module.exports = config;
+
+if (process.env.NODE_ENV === 'production') { // if we're in production mode, here's what happens next
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(), // call the uglify plugin
+        new OptimizeCSSAssets() // call the css optimizer (minfication)
+    );
+}
